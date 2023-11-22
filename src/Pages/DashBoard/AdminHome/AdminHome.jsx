06ffd79b612813643db5c1fc61a2847b55422b32
jillurcf <jrtushar@gmail.comm>
2023-11-21 17:51:51 -0800

    admin home page created and showon data on stats

--- src/Pages/DashBoard/AdminHome/AdminHome.jsx
+++ src/Pages/DashBoard/AdminHome/AdminHome.jsx
@@ -1,8 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/useAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { FaBook, FaDollarSign, FaUnity, FaUsers } from "react-icons/fa";
 
 
 const AdminHome = () => {
-    const {user} = useAuth()
+    const {user} = useAuth();
+   const axiosSecure = useAxiosSecure();
+
+    const {data: stats} = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async ()=> {
+            const res = await axiosSecure.get('/admin-stats');
+            return res.data;
+        }
+    })
+    console.log(stats);
     return (
         <div>
             <h2 className="text-3xl">
@@ -11,6 +24,46 @@ const AdminHome = () => {
                     user?.displayName ? user.displayName : 'Back'
                 }
             </h2>
+
+            <div className="stats shadow">
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+      <FaDollarSign className="text-3xl"></FaDollarSign>
+    </div>
+    <div className="stat-title">Revenue</div>
+    <div className="stat-value">${stats.revenue}</div>
+    <div className="stat-desc"></div>
+  </div>
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+      <FaUsers className="text-3xl"></FaUsers>
+    </div>
+    <div className="stat-title">Users</div>
+    <div className="stat-value">{stats.users}</div>
+    <div className="stat-desc">↗︎ 400 (22%)</div>
+  </div>
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+      {/* <FaUsers className="text-3xl"></FaUsers> */}
+      <FaBook className="text-3xl"></FaBook>
+    </div>
+    <div className="stat-title">Menu Items</div>
+    <div className="stat-value">{stats.menuItems}</div>
+    <div className="stat-desc">↗︎ 400 (22%)</div>
+  </div>
+  
+  <div className="stat">
+    <div className="stat-figure text-secondary">
+   <FaUnity className="text-3xl"></FaUnity>
+    </div>
+    <div className="stat-title">Orders</div>
+    <div className="stat-value">{stats.orders}</div>
+    <div className="stat-desc">↘︎ 90 (14%)</div>
+  </div>
+  
+</div>
         </div>
     );
 };
